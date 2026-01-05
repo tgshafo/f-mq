98712335298f709346cafbc7dac6db08b362c809
tgshafo <uesxa225@gmail.com>
2026-01-06 00:37:00 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 const members = [
     {
         id: 1,
-        nickname: "шафо",
+        nickname: "АКК УТЕРЯН",
         username: "@nothevo",
         category: "Владелец",
         role: "Владелец",
